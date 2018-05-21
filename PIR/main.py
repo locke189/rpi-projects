b09c676bc166c4c6ab3793be3cfd2542d0e78a28
Juan Insuasti <juan.insuasti@gmail.com>
2018-05-20 21:59:59 -0500

    initial commit

--- PIR/main.py
+++ PIR/main.py
@@ -2,3 +2,25 @@ try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+from time import sleep
+
+print('setting everything up!')
+
+# BCM -> broadcom pinout
+# Board -> raspberry pi board piout
+
+GPIO.setmode(GPIO.BOARD)
+
+
+# Channel setup pin 2!
+channel = 2
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+# Polling loop
+while True:
+    if GPIO.input(channel):
+        print('Input was HIGH')
+    else:
+        print('Input was LOW')
+    sleep(1)
