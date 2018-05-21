1f95d2cd4cd8136d389ea26b878ea307557e160e
Juan Insuasti <juan.insuasti@gmail.com>
2018-05-21 07:13:31 -0500

    initial commit

--- PIR/main.py
+++ PIR/main.py
@@ -1,9 +1,11 @@
 try:
     import RPi.GPIO as GPIO
+    from picamera import PiCamera
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
 from time import sleep
+import datetime
 
 print('setting everything up!')
 
@@ -12,15 +14,45 @@ print('setting everything up!')
 
 GPIO.setmode(GPIO.BOARD)
 
+# Camera on!
+camera = PiCamera()
 
 # Channel setup pin 37 -> GPIO26!
-channel = 37 
+# Can also specify a lot of channels using a list
+channel = 37
 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+def snapshot(channel):
+    camera.start_preview()
+    sleep(5)
+    camera.capture('./image_%s.jpg' % getTime())
+    camera.stop_preview()
+
+GPIO.add_event_detect(channel, GPIO.RISING, callback=snapshot)
+
+try:
+    while True:
+        pass
+except:
+    # Closing up
+    print('Cleaning up!')
+    GPIO.cleanup(channel)
+
+
+'''
 # Polling loop
-while True:
-    if GPIO.input(channel):
-        print('Input was HIGH')
-    else:
-        print('Input was LOW')
-    sleep(1)
+try:
+    while True:
+        if GPIO.input(channel):
+            print('Input was HIGH')
+        else:
+            print('Input was LOW')
+        sleep(1)
+except:
+    # Closing up
+    GPIO.cleanup(channel)
+'''
+
+def getTime():
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H:%M:%S')
+    print st
