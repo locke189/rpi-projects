65f9a3c107448dfe95a471c54546ff0d833ffaa7
Juan Insuasti <juan.insuasti@gmail.com>
2018-05-31 08:14:40 -0500

    initial commit

--- PIR/vision/vision.py
+++ PIR/vision/vision.py
@@ -10,7 +10,7 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate.
-def get_labels_from_img(file):
+def get_labels_from_img(file_name):
 
     # Loads the image into memory
     with io.open(file_name, 'rb') as image_file:
